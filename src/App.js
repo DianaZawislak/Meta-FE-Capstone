d169d441274a4b3713170b116c63504a932888ae
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-09 00:22:26 -0500

    imports of new components to app.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Header from './components/Header';
-
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Book from "./components/Book";
 
 
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="book" element={<Book />} />
+        </Routes>
     <Main/>
-    <Nav/>
     <Header/>
     <Footer/>
     </div>
