a8e06777c3b51a189543d5fd6fbb0172177b8c6e
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-09 00:00:05 -0500

    added sections tags to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import React from "react";
+
 
 function App() {
   return (
     <div className="App">
-      Homepage
+    <Nav/>
+    <Header/>
+    <Footer/>
     </div>
   );
 }
