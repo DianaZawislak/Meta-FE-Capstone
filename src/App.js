9cc05e29aa122de4ea341573615605bcefbd94ed
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-09 00:05:13 -0500

    imported components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import React from "react";
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Header from './components/Header';
+
+
 
 
 function App() {
   return (
     <div className="App">
+    <Main/>
     <Nav/>
     <Header/>
     <Footer/>
