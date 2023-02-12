9f9e1ac8713ec2d15ea5bb5145a7c000cfc5419b
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-12 18:52:27 -0500

    about content

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import about_01 from "../images/about_01.jpg";
-import about_02 from "../images/about_02.jpg";
+
+import MarioAdrianA from "../images/MarioAdrianA.jpg";
+import MarioAdrianB from "../images/MarioAdrianB.jpg";
 
 
 const About = () => {
     return (
         <>
             <section className="about-section" id="about">
+                <div className="container">
                 <div className="row">
-                    <div className="col-lg-6 about-images">
-                        <img  className="about-image one" src={about_01} alt="about one" />
-                        <img  className="about-image two" src={about_02} alt="about two" />
-                    </div>
+
                     <div className="col-lg-6 about-details">
                         <h3>Little Lemon</h3>
-                        <h4>Fort Collins</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque vel, voluptate quod
-                        dignissimos sapiente maxime expedita consequuntur labore quo cum ex laboriosam
-                        dignissimos sapiente maxime expedita consequuntur labore quo cum ex laboriosam
-                        incidunt officiis corrupti fugit in dolores ullam.</p>
+                        <h4>Chicago</h4>
+                        <p>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails 
+                            in a lively but casual environment. The restaurant features a locally sourced
+                            menu with daily specials.
+                        </p>
+                        <p>
+                            Little Lemon started out of a family tradition of passion for Greek
+                            cuisine and its place in the rich culinary tradition of the Mediterranean.
+                        </p>
+                        <p>
+                            The company story can be traced back to Athens, Greece more than a
+                            century ago. There, our great-grandparents started what would become a
+                            long line of family bakeries that prepared
+                            traditional baked goods that not
+                            only sustained the local community, but became part of it.
+                        </p>
                     </div>
+                    <div className="col-lg-6 about-images">
+                        <img  className="about-image one" src={MarioAdrianA} alt="about one" />
+                        <img  className="about-image two" src={MarioAdrianB} alt="about two" />
+                    </div>
+                </div>
                 </div>
             </section>
         </>
