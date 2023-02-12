d628a0aab8553a4a1634f249ed7233f57e856c37
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-12 18:52:39 -0500

    footer style and content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,123 @@
 import React from 'react';
-import footer_img from "../images/logo.png"
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBCol,
+  MDBRow,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
-const Footer = () => {
-    return (
-        <>
-            <section className="footer-section" id="footer">
-                <img  className="footer-image" src={footer_img} alt="logo" />
-                <div className="row">
-                    {/* <div className="col-lg-3 footer-logo">
-                        <img  className="footer-image" src={footer_img} alt="logo" />
-                    </div> */}
-                    <div className="col-lg-4 footer-nav">
-                        <h3>Navigation</h3>
-                        <p>Home</p>
-                        <p>About</p>
-                        <p>Menu</p>
-                        <p>Reservation</p>
-                        <p>Order Online</p>
-                    </div>
-                    <div className="col-lg-4 footer-contact">
-                        <h3>Contact</h3>
-                        <p>950 Everest Way, Suite#120</p>
-                        <p>Fort Collins, CO, 80020</p>
-                    </div>
-                    <div className="col-lg-4 social-media">
-                        <h3>Social-Media</h3>
-                        <p>Facebook</p>
-                        <p>Twitter</p>
-                        <p>Instagram</p>
-                        <p>Youtube</p>
-                        <p>Trip Advisor</p>
-                    </div>
-                </div>
-                <p>Copy Rights &copy; 2023 Little Lemon | All Right Reserved.</p>
-            </section>
-        </>
-    );
-};
+import logofoot from "../images/logofoot.png";
 
-export default Footer;
+
+export default function App() {
+  return (
+
+    <MDBFooter className='footer'>
+      <MDBContainer className='p-1'>
+        <section className='socials'>
+
+          <MDBBtn floating className='m-3' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn floating className='m-3' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn  floating className='m-3' href='#!' role='button'>
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+
+          <MDBBtn  floating className='m-3' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+        </section>
+
+        <section className='flinks'>
+          <MDBRow>
+          <MDBCol lg='3'  className='flogo'>
+              <img src={logofoot} alt='logo' height='130px'></img>
+            </MDBCol>
+
+            <MDBCol md='12' lg='3' xl='3' className='flinks'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled'>
+                
+                <li>
+                  <a href='#!' className='footlink'>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='footlink'>
+                    Menu
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='footlink'i>
+                    About
+                  </a>
+                </li>
+               
+              </ul>
+            </MDBCol>
+
+            <MDBCol md='12' lg='3' xl='3'  className='flinks'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled'>
+                
+                <li>
+                  <a href='#!' className='footlink'>
+                    Reserve a Table
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='footlink'>
+                    Sign Up
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='footlink'i>
+                    Login
+                    </a>
+                </li>
+             
+              </ul>
+            </MDBCol>
+
+        
+
+            <MDBCol md='12' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
+            <h5 className='text-uppercase'>Contact</h5>
+              <p>
+                <MDBIcon color='secondary' icon='home' className='me-3' />
+                Chicago, IL 10012, US
+              </p>
+              <p>
+                <MDBIcon color='secondary' icon='envelope' className='me-3' />
+                info@example.com
+              </p>
+              <p>
+                <MDBIcon color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
+              </p>
+              <p>
+                <MDBIcon color='secondary' icon='print' className='me-3' /> + 01 234 567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright: Little Lemon
+        
+      </div>
+    
+    </MDBFooter>
+  );
+}
