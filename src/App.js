2182fa4698c75520e3aea70d02a324e522f4a127
Diana Zawislak <diana.zawislak@icloud.com>
2023-03-14 00:34:28 -0400

    changes for correct deployment

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" component={<Home />} />
           <Route path="/about" element={ <About />} />
           <Route path="/menu" element={ <Menu />} />
           <Route path="/reservation" element={<Reservation />} />
